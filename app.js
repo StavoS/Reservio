89243e1768bb02714d0be081dacd7417a15478de
StavoS <StavSinai@gmail.com>
2024-11-25 23:29:53 +0200

    security xss + data sanitization

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express();
 const AppError = require('./utils/appError');
 const rateLimit = require('express-rate-limit');
 const globalErrorHandler = require('./controllers/errorController');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -12,11 +15,14 @@ const limiter = rateLimit({
     windowMs: 60 * 60 * 100,
     message: 'too many requests from this IP, please try again later.',
 });
+app.use(helmet());
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
 app.use('/api', limiter);
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
+app.use(mongoSanitize());
+app.use(xss());
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
