31f4ff6aeefd546087c833a9404e52a26bbea0c5
StavoS <StavSinai@gmail.com>
2024-11-25 23:17:19 +0200

    security rate limiter

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const AppError = require('./utils/appError');
+const rateLimit = require('express-rate-limit');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 100,
+    message: 'too many requests from this IP, please try again later.',
+});
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
+app.use('/api', limiter);
 app.use(express.json());
 
 app.use('/api/v1/tours', tourRouter);
