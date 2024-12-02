5c26da6740b31f9fe6ea00bccfd104fbac394e76
StavoS <StavSinai@gmail.com>
2024-12-02 23:24:22 +0200

    virtual tour populating

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,35 +1,45 @@
 const mongoose = require('mongoose');
-const reviewSchema = mongoose.Schema({
-    review: {
-        type: String,
-        required: [true, 'review cannot be empty'],
+const reviewSchema = new mongoose.Schema(
+    {
+        review: {
+            type: String,
+            required: [true, 'review cannot be empty'],
+        },
+        rating: {
+            type: Number,
+            min: 1,
+            max: 5,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+        },
+        tour: {
+            type: mongoose.Schema.ObjectId,
+            ref: 'Tour',
+            required: [true, 'Review must belong to a tour.'],
+        },
+        author: {
+            type: mongoose.Schema.ObjectId,
+            ref: 'User',
+            required: [true, 'Review must have an author.'],
+        },
     },
-    rating: {
-        type: Number,
-        min: 1,
-        max: 5,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now(),
-    },
-    tour: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'Tour',
-        required: [true, 'Review must belong to a tour.'],
-    },
-    author: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'User',
-        required: [true, 'Review must have an author.'],
-    },
-});
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true },
+    }
+);
 
 reviewSchema.pre(/^find/, function (next) {
+    // this.populate({
+    //     path: 'tour',
+    //     select: 'name',
+    // }).populate({
+    //     path: 'author',
+    //     select: 'name photo',
+    // });
     this.populate({
-        path: 'tour',
-        select: 'name',
-    }).populate({
         path: 'author',
         select: 'name photo',
     });
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,76 +1,66 @@
 const mongoose = require('mongoose');
 // const User = require('./userModel');
 
-const tourSchema = mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'a tour must have a name'],
-        unique: true,
-        trim: true,
-        maxLength: [40, 'name cant surpass 40 characters.'],
-        minLength: [6, 'name must be 6 characters and above'],
-    },
-    rating: {
-        type: Number,
-        default: 3.5,
-    },
-    price: {
-        type: Number,
-        required: [true, 'a tour must have a price'],
-    },
-    duration: {
-        type: Number,
-        required: [true, 'a tour must have a duration'],
-    },
-    maxGroupSize: {
-        type: Number,
-        required: [true, 'a tour must have a group size'],
-    },
-    difficulty: {
-        type: String,
-        required: [true, 'a tour must have a difficulty'],
-    },
-    ratingsAverage: {
-        type: Number,
-        default: 0,
-    },
-    ratingsQuantity: {
-        type: Number,
-        default: 0,
-    },
-    priceDiscount: Number,
-    summary: {
-        type: String,
-        trim: true,
-        required: [true, 'a tour must have a summary'],
-    },
-    description: {
-        type: String,
-        trim: true,
-    },
-    imageCover: {
-        type: String,
-        required: [true, 'a tour must have an image'],
-    },
-    images: [String],
-    createAt: {
-        type: Date,
-        default: Date.now(),
-        select: false,
-    },
-    startDates: [Date],
-    startLocation: {
-        type: {
+const tourSchema = mongoose.Schema(
+    {
+        name: {
             type: String,
-            default: 'Point',
-            enum: ['Point'],
+            required: [true, 'a tour must have a name'],
+            unique: true,
+            trim: true,
+            maxLength: [40, 'name cant surpass 40 characters.'],
+            minLength: [6, 'name must be 6 characters and above'],
         },
-        coordinates: [Number],
-        address: String,
-        description: String,
-    },
-    locations: [
-        {
+        rating: {
+            type: Number,
+            default: 3.5,
+        },
+        price: {
+            type: Number,
+            required: [true, 'a tour must have a price'],
+        },
+        duration: {
+            type: Number,
+            required: [true, 'a tour must have a duration'],
+        },
+        maxGroupSize: {
+            type: Number,
+            required: [true, 'a tour must have a group size'],
+        },
+        difficulty: {
+            type: String,
+            required: [true, 'a tour must have a difficulty'],
+        },
+        ratingsAverage: {
+            type: Number,
+            default: 0,
+        },
+        ratingsQuantity: {
+            type: Number,
+            default: 0,
+        },
+        priceDiscount: Number,
+        summary: {
+            type: String,
+            trim: true,
+            required: [true, 'a tour must have a summary'],
+        },
+        description: {
+            type: String,
+            trim: true,
+        },
+        imageCover: {
+            type: String,
+            required: [true, 'a tour must have an image'],
+        },
+        images: [String],
+        createAt: {
+            type: Date,
+            default: Date.now(),
+            select: false,
+        },
+        startDates: [Date],
+        startLocation: {
             type: {
                 type: String,
                 default: 'Point',
@@ -79,16 +69,32 @@ const tourSchema = mongoose.Schema({
             coordinates: [Number],
             address: String,
             description: String,
-            day: Number,
         },
-    ],
-    guides: [
-        {
-            type: mongoose.Schema.ObjectId,
-            ref: 'User',
-        },
-    ],
-});
+        locations: [
+            {
+                type: {
+                    type: String,
+                    default: 'Point',
+                    enum: ['Point'],
+                },
+                coordinates: [Number],
+                address: String,
+                description: String,
+                day: Number,
+            },
+        ],
+        guides: [
+            {
+                type: mongoose.Schema.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true },
+    }
+);
 
 tourSchema.pre(/^find/, async function (next) {
     this.populate({
@@ -98,6 +104,12 @@ tourSchema.pre(/^find/, async function (next) {
     next();
 });
 
+//Virtual Populate
+tourSchema.virtual('reviews', {
+    ref: 'Review',
+    foreignField: 'tour',
+    localField: '_id',
+});
 // tourSchema.pre('save', async function (next) {
 //     const guidesPromises = this.guides.map(
 //         async (id) => await User.findById(id)
